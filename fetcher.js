2d6f711f0294c635862301f7f88860c95039b1b3
Pabiak <pabiak17@gmail.com>
2023-03-12 23:55:40 +0100

    Add getRiverDataById function

--- fetcher.js
+++ fetcher.js
@@ -1,27 +1,34 @@
 const fs = require("fs");
 const filteredData = [];
 
-const id1 = "152200020";
-const id2 = "152200120";
-
+const stations = ["152200020", "152200120"]
+const months = ['11', '12', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+const years = ['2015', '2016', '2017']
 const folderPath = "./files";
 const files = fs.readdirSync(folderPath);
 
+
 files.forEach((file) => {
   fs.readFile(`${folderPath}/${file}`, "utf-8", (err, data) => {
+
     if (err) console.log(err);
     const lines = data.split("\n");
-    lines.forEach((line) => {
-      if (line.includes(id1) || line.includes(id2)) {
-        filteredData.push(line);
-      }
-    });
+
+    filteredData.push(lines.filter((line) => {
+      return getRiverDataById(line, stations[0]) || getRiverDataById(line, stations[1]);
+    }));
 
     if (files.indexOf(file) === files.length - 1) {
       saveToFile();
     }
+
   });
 });
+
+const getRiverDataById = (line, id) => {
+  if (!line.includes(id)) return;
+  return line;
+}
 
 const saveToFile = () => {
   const output = filteredData.join("\n");
