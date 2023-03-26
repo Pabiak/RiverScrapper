695310ebeeaa1a0de436a87fe3a98815ce944a93
Pabiak <pabiak17@gmail.com>
2023-03-26 20:17:28 +0200

    modify delete.js to delete generated charts

--- delete.js
+++ delete.js
@@ -1,12 +1,13 @@
 import fs from "fs";
 
-const filePath = "./outputFiles";
-
-fs.readdir(filePath, (err, files) => {
-  if (err) console.log(err);
-  for (const file of files) {
-    fs.unlink(`${filePath}/${file}`, (err) => {
-      if (err) console.log(err);
-    });
-  }
-});
+const filePaths = ["./outputFiles","./charts"];
+for(const filePath of filePaths){
+  fs.readdir(filePath, (err, files) => {
+    if (err) console.log(err);
+    for (const file of files) {
+      fs.unlink(`${filePath}/${file}`, (err) => {
+        if (err) console.log(err);
+      });
+    }
+  });
+}
